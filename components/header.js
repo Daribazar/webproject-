28ae209e1ce2285fa67aa92f29237ff00445f4f6
Batmyagmar <149484739+Bataa715@users.noreply.github.com>
2025-01-04 22:20:36 +0800

    Update header.js
    Signed-off-by: Batmyagmar <149484739+Bataa715@users.noreply.github.com>

--- components/header.js
+++ components/header.js
@@ -5,49 +5,55 @@ class HeaderComponent extends HTMLElement {
 
     shadow.innerHTML = `
       <style>    
-    header {
-        width: 100%;
-        padding: 0px;
-        background-color: #D8A7A1;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        color: #3B1F1A;
-        font-weight: italic;
-        font-family: cursive;
-    }
+        header {
+            width: 100%;
+            padding: 0px;
+            background-color: #D8A7A1;
+            display: flex;
+            justify-content: space-aroundff;
+            align-items: center;
+            color: #3B1F1A;
+            font-weight: italic;
+            font-family: cursive;
+        }
+  
+        header nav a {
+            margin: 0 15px;
+            color: #3B1F1A;
+            text-decoration: none;
+            font-size: 18px;
+            transition: color 0.2s;
+        }
+  
+        header nav a:hover {
+            color: #7B4B47;
+        }
+  
+        header .iconcard {
+            position: relative;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+  
+        header .iconcard span {
+            display: flex;
+            height: 25px;
+            width: 25px;
+            background-color: #F5E1DC;
+            justify-content: center;
+            align-items: center;
+            color: #000;
+            border-radius: 50%;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 14px;
+            font-weight: bold;
+            position: absolute;
+            top: -10px;
+            right: -10px;
+        }
 
-    header nav a {
-        margin: 0 15px;
-        color: #3B1F1A;
-        text-decoration: none;
-        font-size: 18px;
-        transition: color 0.2s;
-    }
-
-    header nav a:hover {
-        color: #7B4B47;
-    }
-
-    header .iconcard {
-        position: relative;
-        cursor: pointer;
-    }
-
-    header .iconcard span {
-        display: flex;
-        height: 25px;
-        width: 25px;
-        background-color: #F5E1DC;
-        justify-content: center;
-        align-items: center;
-        color: #000;
-        border-radius: 50%;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        font-family: Arial, Helvetica, sans-serif;
-    }
         .cart-total {
             color: #3B1F1A;
             font-size: 16px;
@@ -60,7 +66,7 @@ class HeaderComponent extends HTMLElement {
         </a>
         <nav>
             <a href="home.html">Нүүр</a>
-            <a href="app.html">Бүтээгдэхүүн</a>
+            <a href="bvteegdehvvn.html">Бүтээгдэхүүн</a>
             <a href="salbar.html">Салбарын мэдээлэл</a>
             <a href="login.html">Нэвтрэх</a>
         </nav>
@@ -69,6 +75,7 @@ class HeaderComponent extends HTMLElement {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z"/>
             </svg>
             <span id="cart-counter">0</span>
+            <div class="cart-total" id="cart-total">0₮</div>
         </div>
       </header>
     `;
