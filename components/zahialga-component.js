e5e9bc62174a38cccdc68cce15bd6dc6831ea714
odkono1010 <odkosb1010@gmail.com>
2025-01-07 22:02:49 +0800

    fh

--- components/zahialga-component.js
+++ components/zahialga-component.js
@@ -1,20 +1,24 @@
 class ZahialgaComponent extends HTMLElement {
   constructor() {
     super();
-    // localStorage-аас cartItems болон cartTotal утгуудыг авч байна
     this.cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    this.cartTotal = parseFloat(localStorage.getItem('cartTotal')) || 0;  // Тоон утгаар хөрвүүлж байна
-    this.couponCode = '';  // Купон код
-    this.discount = 0;  // Хөнгөлөлт
+    this.cartTotal = parseFloat(localStorage.getItem('cartTotal')) || 0;
+    this.couponCode = '';
+    this.discount = 0;
+    this.attachShadow({ mode: 'open' });
   }
-
   connectedCallback() {
     this.render();
     this.setupCouponHandler();
   }
-
   render() {
-    this.innerHTML = `
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <style>
+        .zahialga { margin-top: 20px; }
+        .cart-total { font-weight: bold; }
+      </style>
+      
       <h3>Таны сагс</h3>
       <ul>
         ${this.cartItems.map(item => `<li>${item.name} - ₮${item.price}</li>`).join('')}
@@ -23,7 +27,7 @@ class ZahialgaComponent extends HTMLElement {
       <section class="zahialga">
         <article>
           <h3>Захиалга</h3>
-          <h5 id="cart-total">Нийт: ₮${this.cartTotal}</h5>
+          <h5 class="cart-total">Нийт: ₮<span id="cart-total">${this.cartTotal}</span></h5>
         </article>
         <article>
           <label for="coupon">Купон ашиглах:</label>
@@ -31,12 +35,17 @@ class ZahialgaComponent extends HTMLElement {
           <button type="button" id="apply-coupon">Купон ашиглах</button>
         </article>
       </section>
+      <slot name="extra-content">qwerty</slot>
+      <slot name="promotion-content">asdfghjk</slot>
     `;
+    
+    this.shadowRoot.innerHTML = '';
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   setupCouponHandler() {
-    const applyCouponButton = this.querySelector('#apply-coupon');
-    const couponInput = this.querySelector('#coupon');
+    const applyCouponButton = this.shadowRoot.querySelector('#apply-coupon');
+    const couponInput = this.shadowRoot.querySelector('#coupon');
     applyCouponButton.addEventListener('click', () => {
       this.couponCode = couponInput.value.trim();
       this.applyCoupon();
@@ -44,28 +53,23 @@ class ZahialgaComponent extends HTMLElement {
   }
 
   applyCoupon() {
-    // Купон кодыг шалгах логик (жишээ нь "DISCOUNT10")
     if (this.couponCode === 'DISCOUNT10') {
-      this.discount = 0.1;  // 10% хөнгөлөлт
+      this.discount = 0.1;
     } else {
       this.discount = 0;
     }
 
-    // Хөнгөлөлт хэрэглэх
     const discountedPrice = this.cartTotal * (1 - this.discount);
     this.cartTotal = discountedPrice;
 
-    // Хөнгөлөлттэй үнийн дүнг шинэчлэх
     this.render();
 
-    // Хөнгөлөлтийн мэдэгдэл
     if (this.discount > 0) {
       alert('Купон код амжилттай ашиглагдлаа!');
     } else {
       alert('Купон код буруу байна.');
     }
 
-    // localStorage-д шинэ үнийн дүнг хадгалах
     localStorage.setItem('cartTotal', this.cartTotal);
   }
 }
